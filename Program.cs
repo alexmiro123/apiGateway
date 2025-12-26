e5862b8727b9abfb67484d2a79556a1454364fd6
alexmiro123 <117776215+alexmiro123@users.noreply.github.com>
2025-12-26 11:58:11 -0500

    inicio del proyecto

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", false, true);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority =
+            $"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/v2.0";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false 
+        };
+    });
+
+builder.Services.AddOcelot();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ERP", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("ERP");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+await app.UseOcelot();
+app.Run();
